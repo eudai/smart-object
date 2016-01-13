092c806b10cbdd65352e3088485496a749b093a8
Christopher Russ <christopher@epsilon.local>
2016-01-13 13:25:41 -0500

    smart-object

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,10 +1,5 @@
 var app = function(){
 
-	var db = "http://127.0.0.1:5984"
-	$.getJSON(db + '/_all_dbs',function(dbs){
-		console.log(dbs)	
-	})
-
 	var smartObject = new SmartObject({
 		_id: "SmartObject",		
 		_sidebar: [{
